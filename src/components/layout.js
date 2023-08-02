29ae9925200c037bbb2ce08d33a209e1b322d686
Darshan Chobarkar <dchobarkar@gmail.com>
2023-08-02 15:27:23 +0530

    change website design

--- src/components/layout.js
+++ src/components/layout.js
@@ -46,6 +46,23 @@ const Layout = (props) => {
           href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        {/* <!-- Bootstrap --> */}
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous"
+        />
+        {/* <!-- Fonts --> */}
+        <link
+          href="https://fonts.googleapis.com/css?family=Lato:400,900&display=swap"
+          rel="stylesheet"
+        />
+        {/* <!-- Material ui icons - google web fonts --> */}
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
       </Helmet>
 
       <Header />
